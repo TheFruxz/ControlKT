837e42e71686e2a0b4b7ac20bfb9d885e0425bd2
TheFruxz <cedricspitzer@outlook.de>
2022-11-20 23:44:41 +0100

    feature: easy & secured routing inside structure

--- ControlData/src/main/kotlin/de/fruxz/control/UserManagement.kt
+++ ControlData/src/main/kotlin/de/fruxz/control/UserManagement.kt
@@ -1,15 +1,25 @@
 package de.fruxz.control
 
 import de.fruxz.control.data.User
+import de.fruxz.control.permission.RequiresPermission
+import de.fruxz.control.routing.EndPoint
+import java.util.UUID
 
 object UserManagement {
 
-	fun getUser(id: String): User? {
-		return null // TODO
+	fun getUser(username: String): User? = User(UUID.randomUUID(), setOf("test")) // TODO
+
+	fun getUser(id: UUID): User? {
+		return getUser("") // TODO
 	}
 
+	@EndPoint
 	fun hasPermission(id: String, permission: String): Boolean {
 		return getUser(id)?.permissions?.contains(permission) == true
 	}
+
+	@EndPoint
+	@RequiresPermission("test")
+	fun test(): String = "This is a test!"
 
 }
